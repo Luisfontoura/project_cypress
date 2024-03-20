90de3c110c4feac6208baa22784d046216168dfd
Luis Fontoura <eve.holt@reqres.in>
2024-03-19 23:42:25 -0300

    subindo alterações

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,9 @@
+// cypress.config.js
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // Configurações gerais do Cypress aqui
-
-  // Configuração específica para testes e2e (ponta a ponta)
   e2e: {
-    // Outras configurações específicas para testes e2e aqui
-    supportFile: "cypress/support/e2e.js" // Substitua pelo caminho real do seu arquivo de suporte para testes e2e
-  },
-
-  // Configuração específica para testes de componentes
-  component: {
-    // Outras configurações específicas para testes de componentes aqui
-    supportFile: "cypress/support/component.js" // Substitua pelo caminho real do seu arquivo de suporte para testes de componentes
+    supportFile: "cypress/support/e2e.js"
   }
 });
