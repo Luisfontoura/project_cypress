84f0fcc5d5778c8d114157af9c52590fc67f4396
Luis Fontoura <eve.holt@reqres.in>
2024-03-29 02:25:31 -0300

    Adicionando novos casos de testes

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -50,4 +50,76 @@ describe('Teste de página local', () => {
         expect(message).to.equal("Campo 'Nome' é obrigatório!") 
       })
     })
-  })
+
+    it('Exibe mensagem de erro ao submeter formulário sem preencher o campo cargo', () => {
+
+      cy.get(ELEMENTS.camponome).type("Leomar Souza")
+      cy.get(ELEMENTS.camposexo).select("Masculino")
+      cy.get(ELEMENTS.campotelefone).type("51999999999")
+      cy.get(ELEMENTS.camposalario).type("5000")
+      cy.get(ELEMENTS.campocpf).type("23062545089")
+      cy.get('.button-primary').click()
+      cy.on('window:alert', (message) => {
+        expect(message).to.equal("Campo 'Cargo' é obrigatório!") 
+      })
+    })
+
+
+
+    it('Exibe mensagem de erro ao submeter formulário sem selecionar o sexo', () => {
+
+      
+      cy.get(ELEMENTS.camponome).type("Leomar Souza")
+      cy.get(ELEMENTS.campocargo).type("QA")
+      cy.get(ELEMENTS.campotelefone).type("51999999999")
+      cy.get(ELEMENTS.camposalario).type("5000")
+      cy.get(ELEMENTS.campocpf).type("23062545089")
+      cy.get('.button-primary').click()
+      cy.on('window:alert', (message) => {
+        expect(message).to.equal("Campo 'Sexo' é obrigatório!") 
+      })
+    })
+
+    it('Exibe mensagem de erro ao submeter formulário sem preencher campo telefone', () => {
+
+      
+      cy.get(ELEMENTS.camponome).type("Leomar Souza")
+      cy.get(ELEMENTS.campocargo).type("QA")
+      cy.get(ELEMENTS.camposexo).select("Masculino")
+      cy.get(ELEMENTS.camposalario).type("5000")
+      cy.get(ELEMENTS.campocpf).type("23062545089")
+      cy.get('.button-primary').click()
+      cy.on('window:alert', (message) => {
+        expect(message).to.equal("Campo 'Telefone' é obrigatório!") 
+      })
+    })
+
+    it('Exibe mensagem de erro ao submeter formulário sem preencher campo salário', () => {
+
+      
+      cy.get(ELEMENTS.camponome).type("Leomar Souza")
+      cy.get(ELEMENTS.campocargo).type("QA")
+      cy.get(ELEMENTS.camposexo).select("Masculino")
+      cy.get(ELEMENTS.campotelefone).type("51999999999")
+      cy.get(ELEMENTS.campocpf).type("23062545089")
+      cy.get('.button-primary').click()
+      cy.on('window:alert', (message) => {
+        expect(message).to.equal("Campo 'Salário' é obrigatório!") 
+      })
+    })
+
+
+    it('Exibe mensagem de erro ao submeter formulário sem preencher campo cpf', () => {
+
+      
+      cy.get(ELEMENTS.camponome).type("Leomar Souza")
+      cy.get(ELEMENTS.campocargo).type("QA")
+      cy.get(ELEMENTS.camposexo).select("Masculino")
+      cy.get(ELEMENTS.campotelefone).type("51999999999")
+      cy.get(ELEMENTS.camposalario).type("5000")
+      cy.get('.button-primary').click()
+      cy.on('window:alert', (message) => {
+        expect(message).to.equal("Campo 'CPF' é obrigatório!") 
+      })
+    })
+})
